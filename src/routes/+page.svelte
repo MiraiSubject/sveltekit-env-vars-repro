50f59839ab3ac6bdac1ff3cff8c5b59178b21b40
Shivam <s.r.v.ramkisoen@umail.leidenuniv.nl>
2023-05-02 20:18:45 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import type { PageData } from "./$types";
+
+  export let data: PageData;
+</script>
+
+<svelte:head>
+  <title>Home</title>
+  <meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+  {#each Object.keys(data.env) as key}
+	<p>{key}</p>
+  {/each}
+</section>
+
+<style>
+  section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    flex: 0.6;
+  }
+</style>
